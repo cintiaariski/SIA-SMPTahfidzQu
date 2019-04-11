32d6a585c868b608e59b0caa1c4b38456d2dd687
cintiaariski <cintiaariski031@gmail.com>
2019-04-12 00:02:47 +0700

    Add files via upload

---
+++ helpers/tanggal_helper.php
@@ -0,0 +1,13 @@
+<?php
+function konversi_bulan($bln)
+{
+	$bulan = array(1=>"Januari",2=>"Februari", 3=>"Maret", 4=>"April", 5=>"Mei", 6=>"Juni", 7=>"Juli", 8=>"Agustus", 9=>"September", 10=>"Oktober", 11=>"November", 12=>"Desember");
+	return $bulan[(int)$bln];
+}
+
+function hari(){
+	$hari = array("Senin","Selasa","Rabu","Kamis","Jumat","Sabtu","Minggu");
+	return $hari;
+}
+
+ ?>
